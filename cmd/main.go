c73a34a21971250caaf5a032cf567d722acfaffc
Ethan Hosier <ethanjhosier@gmail.com>
2025-06-29 14:50:28 +0100

    feat: refac

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	"github.com/ethanhosier/go-module-test/custom_math"
-	"github.com/ethanhosier/go-module-test/hello_world"
+	"github.com/ethanhosier/go-module-test/packages/custom_math"
+	"github.com/ethanhosier/go-module-test/packages/hello_world"
 )
 
 func main() {
